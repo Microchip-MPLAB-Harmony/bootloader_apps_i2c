f47b07fd82625c660d5b2923833c830cf06c06d6
Amit Raddi <amit.raddi@microchip.com>
2020-07-07 10:10:05 +0530

    [I2C] Regenerated demo for i2c_bootloader_test_app_sam_c21n_xpro
    MH3-37901
    
    Signed-off-by: Amit Raddi <amit.raddi@microchip.com>

--- apps/i2c_bootloader/test_app/firmware/src/app.h
+++ apps/i2c_bootloader/test_app/firmware/src/app.h
@@ -196,7 +196,7 @@ void APP_Tasks( void );
 
 
 
-#endif /* _APP_H */
+
 
 //DOM-IGNORE-BEGIN
 #ifdef __cplusplus
@@ -204,6 +204,7 @@ void APP_Tasks( void );
 #endif
 //DOM-IGNORE-END
 
+#endif /* _APP_H */
 /*******************************************************************************
  End of File
  */
