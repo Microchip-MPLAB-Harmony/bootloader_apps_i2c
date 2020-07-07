39a4f8b590062f5e37134df8197d1370ed3edcc7
Amit Raddi <amit.raddi@microchip.com>
2020-07-07 10:21:58 +0530

    [I2C_FAIL_SAFE] Regenerated demo for i2c_fail_safe_bootloader_bootloader_sam_e54_xpro
    MH3-37927
    
    Signed-off-by: Amit Raddi <amit.raddi@microchip.com>

--- apps/i2c_fail_safe_bootloader/bootloader/firmware/src/config/sam_e54_xpro/peripheral/port/plib_port.c
+++ apps/i2c_fail_safe_bootloader/bootloader/firmware/src/config/sam_e54_xpro/peripheral/port/plib_port.c
@@ -76,16 +76,20 @@ void PORT_Initialize(void)
    PORT_REGS->GROUP[0].PORT_PINCFG[23] = 0x1;
 
    PORT_REGS->GROUP[0].PORT_PMUX[11] = 0x22;
+                
    /************************** GROUP 1 Initialization *************************/
    PORT_REGS->GROUP[1].PORT_OUT = 0x80000000;
    PORT_REGS->GROUP[1].PORT_PINCFG[31] = 0x6;
 
+                
    /************************** GROUP 2 Initialization *************************/
    PORT_REGS->GROUP[2].PORT_DIR = 0x40000;
    PORT_REGS->GROUP[2].PORT_OUT = 0x40000;
 
+                
    /************************** GROUP 3 Initialization *************************/
 
+                
 }
 
 // *****************************************************************************
