42c0461600602ad098b868c83df55cda504868eb
Sumanth Naga Venkata Ranga Suraneni <sumanth.suraneni@microchip.com>
2023-12-11 18:50:40 +0530

    [I2C] Regenerated demo for i2c_bootloader_bootloader_pic32cz_ca80_curiosity_ultra
    MH3-88277

--- apps/i2c_bootloader/bootloader/firmware/src/config/pic32cz_ca80/startup_xc32.c
+++ apps/i2c_bootloader/bootloader/firmware/src/config/pic32cz_ca80/startup_xc32.c
@@ -28,6 +28,9 @@
 #include <sys/cdefs.h>
 #include <stdbool.h>
 
+/* MISRAC 2012 deviation block start */
+/* MISRA C-2012 Rule 21.2 deviated 1 times. Deviation record ID -  H3_MISRAC_2012_R_21_2_DR_1 */
+/* MISRA C-2012 Rule 8.6 deviated 7 times.  Deviation record ID -  H3_MISRAC_2012_R_8_6_DR_1 */
 /* Initialize segments */
 extern uint32_t _sfixed;
 extern void _ram_end_(void);
@@ -37,6 +40,7 @@ extern int main(void);
 
 /* Declaration of Reset handler (may be custom) */
 void __attribute__((noinline)) Reset_Handler(void);
+extern void (* const vectors[])(void);
 
 __attribute__ ((used, section(".vectors")))
 void (* const vectors[])(void) =
@@ -53,28 +57,52 @@ void (* const vectors[])(void) =
 /* Linker-defined symbols for data initialization. */
 extern uint32_t _sdata, _edata, _etext;
 extern uint32_t _sbss, _ebss;
+/* MISRAC 2012 deviation block end */
 
 void __attribute__((noinline, section(".romfunc.Reset_Handler"))) Reset_Handler(void)
 {
+    register uint32_t count;
+/* MISRAC 2012 deviation block start */
+/* MISRA C-2012 Rule 18.1 deviated 1 times. Deviation record ID -  H3_MISRAC_2012_R_18_1_DR_1 */
+    register uint64_t *pFlexRam = (uint64_t*)(uintptr_t)&_sdata;
+
+    // FlexRAM initialization loop (to handle ECC properly)
+    // we need to initialize all of RAM with 64 bit aligned writes
+    for (count = 0U; count < (((uint32_t)&_ram_end_ - (uint32_t)&_sdata) / 8U); count++)
+    {
+        pFlexRam[count] = 0U;
+    }
+
+    __DSB();
+    __ISB();
+/* MISRAC 2012 deviation block end */
 
     uint32_t *pSrc, *pDst;
+    uintptr_t src, dst;
 
 
-    pSrc = (uint32_t *) &_etext; /* flash functions start after .text */
-    pDst = (uint32_t *) &_sdata;  /* boundaries of .data area to init */
+    src = (uintptr_t)&_etext;
+    pSrc = (uint32_t *)src;      /* flash functions start after .text */
+    dst = (uintptr_t)&_sdata;
+    pDst = (uint32_t *)dst;      /* boundaries of .data area to init */
 
     /* Init .data */
-    while (pDst < &_edata)
-        *pDst++ = *pSrc++;
+    for (count = 0U; count < (((uint32_t)&_edata - (uint32_t)dst) / 4U); count++)
+    {
+        pDst[count] = pSrc[count];
+    }
 
     /* Init .bss */
-    pDst = &_sbss;
-    while (pDst < &_ebss)
-      *pDst++ = 0;
+    dst = (uintptr_t)&_sbss;
+    pDst = (uint32_t *)dst;
+    for (count = 0U; count < (((uint32_t)&_ebss - (uint32_t)dst) / 4U); count++)
+    {
+        pDst[count] = 0U;
+    }
 
 
      /* Branch to application's main function */
-    main();
+    (void)main();
 
 #if (defined(__DEBUG) || defined(__DEBUG_D)) && defined(__XC32)
     __builtin_software_breakpoint();
