8e75e52a7b847745857fca934a1d18310f9cdc9b
Sumanth Naga Venkata Ranga Suraneni <sumanth.suraneni@microchip.com>
2023-12-11 19:04:38 +0530

    [I2C] Regenerated demo for i2c_bootloader_host_app_nvm_sam_l21_xpro
    MH3-88301

--- apps/i2c_bootloader/host_app_nvm/firmware/src/config/sam_l21_xpro/definitions.h
+++ apps/i2c_bootloader/host_app_nvm/firmware/src/config/sam_l21_xpro/definitions.h
@@ -51,11 +51,11 @@
 #include "peripheral/nvmctrl/plib_nvmctrl.h"
 #include "peripheral/sercom/i2c_master/plib_sercom2_i2c_master.h"
 #include "peripheral/evsys/plib_evsys.h"
-#include "peripheral/pm/plib_pm.h"
 #include "peripheral/port/plib_port.h"
 #include "peripheral/clock/plib_clock.h"
 #include "peripheral/nvic/plib_nvic.h"
 #include "peripheral/pac/plib_pac.h"
+#include "peripheral/pm/plib_pm.h"
 #include "peripheral/dsu/plib_dsu.h"
 
 // DOM-IGNORE-BEGIN
@@ -65,6 +65,12 @@ extern "C" {
 
 #endif
 // DOM-IGNORE-END
+
+/* Device Information */
+#define DEVICE_NAME			 "ATSAML21J18B"
+#define DEVICE_ARCH			 "CORTEX-M0PLUS"
+#define DEVICE_FAMILY		 "SAML"
+#define DEVICE_SERIES		 "SAML21"
 
 /* CPU clock frequency */
 #define CPU_CLOCK_FREQUENCY 48000000
