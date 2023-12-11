0c6c4bb72c7fb153756d911a9b1ea476e3e7d980
Sumanth Naga Venkata Ranga Suraneni <sumanth.suraneni@microchip.com>
2023-12-11 19:05:43 +0530

    [I2C] Regenerated demo for i2c_bootloader_bootloader_sam_l22_xpro
    MH3-88290

--- apps/i2c_bootloader/bootloader/firmware/src/config/sam_l22_xpro/definitions.h
+++ apps/i2c_bootloader/bootloader/firmware/src/config/sam_l22_xpro/definitions.h
@@ -50,11 +50,11 @@
 #include <stdbool.h>
 #include "peripheral/nvmctrl/plib_nvmctrl.h"
 #include "peripheral/evsys/plib_evsys.h"
-#include "peripheral/pm/plib_pm.h"
 #include "peripheral/port/plib_port.h"
 #include "peripheral/clock/plib_clock.h"
 #include "peripheral/nvic/plib_nvic.h"
 #include "peripheral/pac/plib_pac.h"
+#include "peripheral/pm/plib_pm.h"
 #include "peripheral/sercom/i2c_slave/plib_sercom5_i2c_slave.h"
 #include "bootloader/bootloader_i2c.h"
 #include "peripheral/dsu/plib_dsu.h"
@@ -66,6 +66,12 @@ extern "C" {
 
 #endif
 // DOM-IGNORE-END
+
+/* Device Information */
+#define DEVICE_NAME			 "ATSAML22N18A"
+#define DEVICE_ARCH			 "CORTEX-M0PLUS"
+#define DEVICE_FAMILY		 "SAML"
+#define DEVICE_SERIES		 "SAML22"
 
 /* CPU clock frequency */
 #define CPU_CLOCK_FREQUENCY 32000000
